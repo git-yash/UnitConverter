3f81af983ce8143ce024ec754337981b192f9478
git-yash <yashmittalshah@gmail.com>
2020-08-12 15:49:15 -0500

    implemented different types of conversion

--- src/UIBuilder.java
+++ src/UIBuilder.java
@@ -1,14 +1,15 @@
 import javax.swing.*;
-import javax.swing.plaf.basic.BasicSplitPaneUI;
 import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 
 public class UIBuilder {
     private final JPanel panel = new JPanel();
     JComboBox<String> unit = new JComboBox<>(Length.units);
     JComboBox<String> unit2 = new JComboBox<>(Length.units);
+    String[] typeOfConversionList = {"Length", "Time", "Temperature"};
+    JComboBox<String> typeOfConversionComboBox = new JComboBox<>(typeOfConversionList);
+    String selectedTypeOfConversion = "Length";
 
     JTextField secondUnit = new JTextField();
 
@@ -30,14 +31,36 @@ public class UIBuilder {
 
     public void createFrame() {
         JTextField firstUnit = createFirstTextField();
-
         JFrame frame = new JFrame("Unit Converter");
 
         panel.setLayout(new GridLayout(2, 2, 30, 0));
         panel.setBounds(15, 75, 350, 85);
 
-        String[] typeOfConversionList = {"Length", "Time", "Temperature"};
-        JComboBox<String> typeOfConversionComboBox = new JComboBox<>(typeOfConversionList);
+        typeOfConversionComboBox.addActionListener(e -> {
+            firstUnit.setText("");
+            secondUnit.setText("");
+            if(typeOfConversionComboBox.getSelectedItem() == "Length"){
+                DefaultComboBoxModel<String> lengthModel = new DefaultComboBoxModel<>(Length.units);
+                DefaultComboBoxModel<String> lengthModel2 = new DefaultComboBoxModel<>(Length.units);
+                unit.setModel(lengthModel);
+                unit2.setModel(lengthModel2);
+                setSelectedTypeOfConversion("Length");
+            }
+            else if(typeOfConversionComboBox.getSelectedItem() == "Time"){
+                DefaultComboBoxModel<String> timeModel = new DefaultComboBoxModel<>(Time.units);
+                DefaultComboBoxModel<String> timeModel2 = new DefaultComboBoxModel<>(Time.units);
+                unit.setModel(timeModel);
+                unit2.setModel(timeModel2);
+                setSelectedTypeOfConversion("Time");
+            }
+            else{
+                DefaultComboBoxModel<String> temperatureModel = new DefaultComboBoxModel<>(Temperature.units);
+                DefaultComboBoxModel<String> temperatureModel2 = new DefaultComboBoxModel<>(Temperature.units);
+                unit.setModel(temperatureModel);
+                unit2.setModel(temperatureModel2);
+                setSelectedTypeOfConversion("Temperature");
+            }
+        });
 
 
         JLabel formula = new JLabel();
@@ -64,12 +87,21 @@ public class UIBuilder {
         JTextField textField = new JTextField();
         textField.addKeyListener(new KeyAdapter() {
             public void keyReleased(KeyEvent e) {
+                String secondNumber;
                 char c = e.getKeyChar();
                 if (((c < '0') || (c > '9')) && (c != KeyEvent.VK_BACK_SPACE) && (c != KeyEvent.VK_PERIOD) && (c != KeyEvent.VK_MINUS)) {
                     e.consume();
                 }
                 try {
-                    String secondNumber = String.valueOf(Length.convertUnit(Double.parseDouble(textField.getText()), getFirstSelectedItem(), getSecondSelectedItem()));
+                    if(selectedTypeOfConversion.equals("Length")) {
+                        secondNumber = String.valueOf(Length.convertUnit(Double.parseDouble(textField.getText()), getFirstSelectedItem(), getSecondSelectedItem()));
+                    }
+                    else if(selectedTypeOfConversion.equals("Time")) {
+                        secondNumber = String.valueOf(Time.convertUnit(Double.parseDouble(textField.getText()), getFirstSelectedItem(), getSecondSelectedItem()));
+                    }
+                    else{
+                        secondNumber = String.valueOf(Temperature.convertUnit(Double.parseDouble(textField.getText()), getFirstSelectedItem(), getSecondSelectedItem()));
+                    }
                     if (secondNumber.endsWith(".0")) {
                         secondNumber = secondNumber.substring(0, secondNumber.length() - 2);
                     }
@@ -80,6 +112,17 @@ public class UIBuilder {
             }
         });
         return textField;
+    }
+
+//    private String getSelectedTypeOfConversion(){
+//        String selectedTypeOfConversion;
+//        if(typeOfConversionComboBox.getSelectedItem() == "Length"){selectedTypeOfConversion = "Length";}
+//        else if(typeOfConversionComboBox.getSelectedItem() == "Time"){selectedTypeOfConversion = "Time";}
+//        else{selectedTypeOfConversion = "Temperature";}
+//        return selectedTypeOfConversion;
+//    }
+    private void setSelectedTypeOfConversion(String type){
+        selectedTypeOfConversion = type;
     }
 
 }
